650d02bfd6b6d706da894ef7967aae36a66796ca
alanoudalhamid <alanoudalhamid@gmail.com>
2019-11-19 10:27:23 +0300

    Key value validation

--- js/inputValidation.js
+++ js/inputValidation.js
@@ -1,56 +1,66 @@
-
-(function ($) {
-    "use strict";
-
-    
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
+function validation() {
+    var input = $('#msgtextarea');
+    var keyObj = $("#key");
+    var keyVal = keyObj.val();
+    var check = true;
+    //validate Key
+    if (keyVal.length < 26) {
+        showValidate(keyObj);
+        check = false;
+    } else if (!allLetters(keyVal)) {
+        showValidate(keyObj);
+        check = false;
+    }
+    //validate message
+    for (var i = 0; i < input.length; i++) {
+        if (validate(input[i]) == false) {
+            showValidate(input[i]);
+            check = false;
         }
+    }
+    if (check == true) {
+        hideValidate(input);
+        hideValidate(keyObj);
+    }
+    return check;
+}
 
-        return check;
+$('.validate-form .input100').each(function() {
+    $(this).focus(function() {
+        hideValidate(this);
     });
+});
 
+function validate(input) {
+    var inputVal = input.value.replace(/\s+$/,"");
+    var letters = /^[a-zA-Z ]*$/;
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-	var letters = /^[a-zA-Z ]*$/;
-
-            if($(input).val().trim() == ''){
-				return false;
-			}
-
-			if(!input.value.match(letters)){
-				return false
-			}
+    if (inputVal.trim() == '') {
+        return false;
     }
 
-    function showValidate(input, caseNumber) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
+    if (!inputVal.match(letters)) {
+        return false
     }
+}
 
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
+function showValidate(input) {
+    var thisAlert = $(input).parent();
 
-        $(thisAlert).removeClass('alert-validate');
+    $(thisAlert).addClass('alert-validate');
+}
+
+function hideValidate(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).removeClass('alert-validate');
+}
+
+function allLetters(str) {
+    let alpha = new Set("abcdefghijklmnopqrstuvwxyz")
+    for (let c of str.toLowerCase()) {
+        alpha.delete(c)
+        if (alpha.size == 0) return true
     }
-    
-    
-
-})(jQuery);
+    return false
+}
